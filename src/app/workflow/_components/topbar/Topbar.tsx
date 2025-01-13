6d140d035bc1483d9f93ad196be2f948261d6326
Navneet Shahi <navneetshahi345@gmail.com>
2025-01-13 23:45:20 +0530

    update Editor.tsx and Topbar.tsx

--- src/app/workflow/_components/topbar/Topbar.tsx
+++ src/app/workflow/_components/topbar/Topbar.tsx
@@ -2,11 +2,17 @@
 
 import TooltipWrapper from "@/components/TooltipWrapper";
 import { Button } from "@/components/ui/button";
+import { subtle } from "crypto";
 import { ChevronLeftIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function Topbar() {
+interface Props {
+  title: string;
+  subTitle?: string;
+}
+
+export default function Topbar({ title, subTitle }: Props) {
   const router = useRouter();
   return (
     <header className="flex p-2 border-p-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10">
@@ -16,6 +22,14 @@ export default function Topbar() {
             <ChevronLeftIcon size={20} />
           </Button>
         </TooltipWrapper>
+        <div className="">
+          <p className="font-bold text-ellipsis truncate">{title}</p>
+          {subTitle && (
+            <p className="text-xs text-muted-foreground text-ellipsis truncate">
+              {subTitle}
+            </p>
+          )}
+        </div>
       </div>
     </header>
   );
