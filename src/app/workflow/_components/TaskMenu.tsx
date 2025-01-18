c36c15f5825b5b240deea26ed3040274a25de832
Navneet Shahi <navneetshahi345@gmail.com>
2025-01-18 23:25:17 +0530

    update TaskMenu.tsx

--- src/app/workflow/_components/TaskMenu.tsx
+++ src/app/workflow/_components/TaskMenu.tsx
@@ -12,7 +12,11 @@ import { TaskType } from "@/types/task";
 function TaskMenu() {
   return (
     <aside className="w-[340px] min-w-[340px] max-w-[340px] border-r-2 border-separate h-full p-2 px-4 overflow-auto">
-      <Accordion type="multiple" className="w-full">
+      <Accordion
+        type="multiple"
+        className="w-full"
+        defaultValue={["extraction"]}
+      >
         <AccordionItem value="extraction">
           <AccordionTrigger className="font-bold">
             Data Extraction
@@ -29,10 +33,22 @@ function TaskMenu() {
 function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
   const task = TaskRegistry[taskType];
 
+  const onDragStart = (event: React.DragEvent, type: TaskType) => {
+    event.dataTransfer.setData("application/reactflow", type);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
   return (
-    <Button>
-      <task.icon size={20} />
-      {task.label}
+    <Button
+      variant={"secondary"}
+      className="flex justify-between gap-2 items-center border w-full"
+      draggable
+      onDragStart={(event) => onDragStart(event, taskType)}
+    >
+      <div className="flex gap-2">
+        <task.icon size={20} />
+        {task.label}
+      </div>
     </Button>
   );
 }
