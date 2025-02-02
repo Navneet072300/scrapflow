822087170606008fb8ddac77e0522b96be6e850b
Navneet Shahi <navneetshahi345@gmail.com>
2025-02-02 23:15:57 +0530

    update executionPlan.ts

--- src/lib/workflow/executionPlan.ts
+++ src/lib/workflow/executionPlan.ts
@@ -1,5 +1,5 @@
 import { AppNode } from "@/types/appNode";
-import { Edge } from "@xyflow/react";
+import { Edge, getIncomers } from "@xyflow/react";
 import { TaskRegistry } from "./task/registry";
 import {
   WorkflowExecutionPlan,
@@ -37,6 +37,27 @@ export function FlowToExecutionPlan(
     phase++
   ) {
     const nextPhase: WorkFlowExecutionPlanPhase = { phase, nodes: [] };
+    for (const currentNode of nodes) {
+      if (planned.has(currentNode.id)) {
+        // Node already put in the execution plan.
+        continue;
+      }
+
+      const invalidInputs = getInvalidInputs(currentNode, edges, planned);
+      if (invalidInputs.length > 0) {
+        const incomers = getIncomers(currentNode, nodes, edges);
+        if (incomers.every((incomer) => planned.has(incomer.id))) {
+          console.error("invalid inputs", currentNode.id, invalidInputs);
+          throw new Error("TODO: HANDLE ERROR 1");
+        } else {
+          // let's skip this node for now
+          continue;
+        }
+      }
+
+      nextPhase.nodes.push(currentNode);
+      planned.add(currentNode.id);
+    }
   }
 
   return { executionPlan };
