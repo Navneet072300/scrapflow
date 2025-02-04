c3a2bbcdfb6585b9a52242008fba9705f49feb01
Navneet Shahi <navneetshahi345@gmail.com>
2025-02-05 00:00:15 +0530

    update NodeCard.tsx and NodeInputs.tsx

--- src/app/workflow/_components/nodes/NodeInputs.tsx
+++ src/app/workflow/_components/nodes/NodeInputs.tsx
@@ -4,6 +4,7 @@ import { Handle, Position, useEdges } from "@xyflow/react";
 import { ReactNode } from "react";
 import NodeParamsField from "./NodeParamsField";
 import { ColorForHandle } from "./common";
+import { useFlowValidation } from "@/components/hooks/useFlowValidation";
 
 export function NodeInputs({ children }: { children: ReactNode }) {
   return <div className="flex flex-col divide-y gap-2">{children}</div>;
@@ -17,12 +18,23 @@ export function NodeInput({
   nodeId: string;
 }) {
   const edges = useEdges();
+  const { invalidInputs } = useFlowValidation();
+
+  const hasErrors = invalidInputs
+    .find((i) => i.nodeId === nodeId)
+    ?.inputs.find((i) => i === input.name);
+
   const isConnected = edges.some(
     (edge) => edge.target === nodeId && edge.targetHandle === input.name
   );
 
   return (
-    <div className="flex justify-start relative p-3 w-full bg-secondary">
+    <div
+      className={cn(
+        "flex justify-start relative p-3 bg-secondary w-full",
+        hasErrors && "bg-destructive/30"
+      )}
+    >
       <NodeParamsField param={input} nodeId={nodeId} disabled={isConnected} />
       {!input.hideHandle && (
         <Handle
